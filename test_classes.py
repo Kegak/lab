06f40aa3a082e4f3675e805187697d8f8ec3f13f
Keegan <keeganjsims@gmail.com>
2022-04-21 16:35:17 -0500

    added checks for when tv is off

--- test_classes.py
+++ test_classes.py
@@ -1,25 +1,35 @@
+import pytest
 from classes import *
 
 class Test:
     def setup_method(self):
         self.on = power(False) #Should set it to True
         self.off = power(True) #Should set it to False
+
     def teardown_method(self):
         del self.on
         del self.off
+
     def test_power(self):
         assert power(False) == True
         assert power(True) == False
 
     def test_channel_up(self):
         assert self.on.channel_up() >= 0 and <= 3
+        with pytest.raises(Exception):
+            self.off.channel_down()
 
     def test_channel_down(self):
         assert self.on.channel_down() >= 0 and <= 3
+        with pytest.raises(Exception):
+            self.off.channel_down()
 
     def test_volume_up(self):
         assert self.on.volume_up() >= 0 and <= 2
+        with pytest.raises(Exception):
+            self.off.volume_up()
 
     def test_volume_down(self):
         assert self.on.volume_down() >= 0 and <= 2
-        
+        with pytest.raises(Exception):
+            self.off.volume_down()
