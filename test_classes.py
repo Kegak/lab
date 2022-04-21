a4f7a21d3191ae015215bc4019c3a2bce91c9fd1
Keegan <keeganjsims@gmail.com>
2022-04-21 15:49:09 -0500

    started coding

---
+++ test_classes.py
@@ -0,0 +1,14 @@
+import pytest
+from classes import *
+
+def test_power():
+    assert power(False) == True
+    assert power(True) == False
+
+def test_channel_up():
+    assert 
+def test_channel_down():
+
+def test_volume_up():
+
+def test_volume_down():
