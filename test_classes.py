94c03f1c690c6857e87b7d53dead4aa9b5f3790d
Keegan <keeganjsims@gmail.com>
2022-04-21 16:46:44 -0500

    Added Type tests

--- test_classes.py
+++ test_classes.py
@@ -17,19 +17,34 @@ class Test:
     def test_channel_up(self):
         assert self.on.channel_up() >= 0 and <= 3
         with pytest.raises(Exception):
-            self.off.channel_down()
+            self.off.channel_up()
+        with pytest.raises(TypeError):
+            assert self.on.channel_up(2)
+            assert self.on.channel_up(1.3)
+            assert self.on.channel_up('')
 
     def test_channel_down(self):
         assert self.on.channel_down() >= 0 and <= 3
         with pytest.raises(Exception):
             self.off.channel_down()
-
+        with pytest.raises(TypeError):
+            assert self.on.channel_down(2)
+            assert self.on.channel_down(1.3)
+            assert self.on.channel_down('')
     def test_volume_up(self):
         assert self.on.volume_up() >= 0 and <= 2
         with pytest.raises(Exception):
             self.off.volume_up()
+        with pytest.raises(TypeError):
+            assert self.on.volume_up(2)
+            assert self.on.volume_up(1.3)
+            assert self.on.volume_up('')
 
     def test_volume_down(self):
         assert self.on.volume_down() >= 0 and <= 2
         with pytest.raises(Exception):
             self.off.volume_down()
+        with pytest.raises(TypeError):
+            assert self.on.volume_down(2)
+            assert self.on.volume_down(1.3)
+            assert self.on.volume_down('')
